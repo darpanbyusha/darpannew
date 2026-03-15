3a73da7b3b1f29b7a31cc483c7ba1da29775033c
darpanbyusha <usha.agg76@gmail.com>
2026-03-15 19:57:00 +0000

    Update products.js

--- products.js
+++ products.js
@@ -5,7 +5,7 @@ const darpanDesigns = [
         name: "Zardozi Silk Blouse",
         category: "blouses",
         collectionName: "Heritage Classics",
-        image: "images/dresses.png",
+        image: "images/dress8.png",
         topRated: true,  // Set to true to show on Homepage
         isNew: true      // For future use
     },
@@ -14,7 +14,7 @@ const darpanDesigns = [
         name: "Banarasi Silk Kurti",
         category: "kurtis",
         collectionName: "Luxury Prêt",
-        image: "https://maps.app.goo.gl/DMc1oQKZqG3JgUqF6",
+        image: "images/dress7.png",
         topRated: true,
         isNew: false
     },
@@ -23,9 +23,118 @@ const darpanDesigns = [
         name: "Bridal Crimson Lehenga",
         category: "lehengas",
         collectionName: "Bridal Couture",
-        image: "https://lh3.googleusercontent.com/aida-public/AB6AXuBkY9KF9T37fmryq3ZANY8-iii9ulTZGu8hBMmUoLd9MhWEUYNSSwZdu_AOFeP6euPocAnf0UldX-PNOG8C5xoSzpO6sMC_bDMzTElyL0fZKtErljnyvKTBHP8AdJzdome62kkabORJZ42867fkeQUbeOXgf_YuVJc6FcKnRQHXl-PGDuiKSWoSCo306zAX8g2V-j8yrV1A-eDWgKt2SCd1OU79dmrTZdbVSf3BHDjC0fWYbpBHSU6Zz5EkI4WBHvfBNagVv_ikclM",
-        topRated: false, // Will only show on the Designs/Collections pages
+        image: "images/dress6.png",
+        topRated: true, // Will only show on the Designs/Collections pages
+        isNew: true
+    },
+    {
+        id: "Blouse-001",
+        name: "Zardozi Silk Blouse",
+        category: "blouses",
+        collectionName: "Heritage Classics",
+        image: "images/dress8.png",
+        topRated: true,  // Set to true to show on Homepage
+        isNew: true      // For future use
+    },
+    {
+        id: "Kurti-002",
+        name: "Banarasi Silk Kurti",
+        category: "kurtis",
+        collectionName: "Luxury Prêt",
+        image: "images/dress7.png",
+        topRated: true,
+        isNew: false
+    },
+    {
+        id: "Lehenga-003",
+        name: "Bridal Crimson Lehenga",
+        category: "lehengas",
+        collectionName: "Bridal Couture",
+        image: "images/dress6.png",
+        topRated: true, // Will only show on the Designs/Collections pages
+        isNew: true
+    },
+    {
+        id: "Blouse-001",
+        name: "Zardozi Silk Blouse",
+        category: "blouses",
+        collectionName: "Heritage Classics",
+        image: "images/dress8.png",
+        topRated: true,  // Set to true to show on Homepage
+        isNew: true      // For future use
+    },
+    {
+        id: "Kurti-002",
+        name: "Banarasi Silk Kurti",
+        category: "kurtis",
+        collectionName: "Luxury Prêt",
+        image: "images/dress7.png",
+        topRated: true,
+        isNew: false
+    },
+    {
+        id: "Lehenga-003",
+        name: "Bridal Crimson Lehenga",
+        category: "lehengas",
+        collectionName: "Bridal Couture",
+        image: "images/dress6.png",
+        topRated: true, // Will only show on the Designs/Collections pages
+        isNew: true
+    },
+    {
+        id: "Blouse-001",
+        name: "Zardozi Silk Blouse",
+        category: "blouses",
+        collectionName: "Heritage Classics",
+        image: "images/dress8.png",
+        topRated: true,  // Set to true to show on Homepage
+        isNew: true      // For future use
+    },
+    {
+        id: "Kurti-002",
+        name: "Banarasi Silk Kurti",
+        category: "kurtis",
+        collectionName: "Luxury Prêt",
+        image: "images/dress7.png",
+        topRated: true,
+        isNew: false
+    },
+    {
+        id: "Lehenga-003",
+        name: "Bridal Crimson Lehenga",
+        category: "lehengas",
+        collectionName: "Bridal Couture",
+        image: "images/dress6.png",
+        topRated: true, // Will only show on the Designs/Collections pages
+        isNew: true
+    },
+    {
+        id: "Blouse-001",
+        name: "Zardozi Silk Blouse",
+        category: "blouses",
+        collectionName: "Heritage Classics",
+        image: "images/dress8.png",
+        topRated: true,  // Set to true to show on Homepage
+        isNew: true      // For future use
+    },
+    {
+        id: "Kurti-002",
+        name: "Banarasi Silk Kurti",
+        category: "kurtis",
+        collectionName: "Luxury Prêt",
+        image: "images/dress7.png",
+        topRated: true,
+        isNew: false
+    },
+    {
+        id: "Lehenga-003",
+        name: "Bridal Crimson Lehenga",
+        category: "lehengas",
+        collectionName: "Bridal Couture",
+        image: "images/dress6.png",
+        topRated: true, // Will only show on the Designs/Collections pages
         isNew: true
     }
     // To add a new design later, just copy one of these blocks, paste it below, and change the details!
+    
 ];
